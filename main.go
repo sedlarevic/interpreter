1b79a866dd2c413b2cfebd31af26c9590407e22e
Stefan Sedlarevic <raldesedlar@gmail.com>
2026-04-29 16:28:17 +0200

    added repl and main where repl starts

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello, %s!\n", user.Username)
+	fmt.Printf("Endulge, type in some commands!\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
