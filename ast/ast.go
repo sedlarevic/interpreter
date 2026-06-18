933358e61ca7491337c87be04a2943fa2d8b2e79
Stefan Sedlarevic <raldesedlar@gmail.com>
2026-06-18 13:09:59 +0200

    parser - function literal parsing support

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"interpreter/token"
+	"strings"
 )
 
 type Node interface {
@@ -154,11 +155,11 @@ type BlockStatement struct {
 	Statements []Statement
 }
 
-func (bs *BlockStatement) StatementNode() {}
+func (bs *BlockStatement) StatementNode()       {}
 func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
 func (bs *BlockStatement) String() string {
 	var out bytes.Buffer
-	for _, s := range bs.Statements{
+	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
 	return out.String()
@@ -179,11 +180,32 @@ func (ie *IfExpression) String() string {
 	out.WriteString(ie.Condition.String())
 	out.WriteString(" ")
 	out.WriteString(ie.Consequence.String())
-
 	if ie.Alternative != nil {
 		out.WriteString("else ")
 		out.WriteString(ie.Alternative.String())
 	}
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 	return out.String()
 }
 
